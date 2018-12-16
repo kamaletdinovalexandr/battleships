944d96976ad17e6b2bd55853a91e4343f83ae0ce
alx <kamaletdinov.alexandr@gmail.com>
2018-12-16 19:46:44 +0300

    init

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	[SerializeField] private Camera _camera;
+	[SerializeField] private GameObject _cellPrefab;
+	[SerializeField] private int _boardWith;
+	[SerializeField] private int _boardHeight;
+	private Cell[,] _cells;
+
+	private bool _isCellSelected;
+	private Vector2Int _currentlySelected;
+
+	private void Start() {
+		_cells = new Cell[_boardWith, _boardHeight];
+		for (int i = 0; i < _boardWith; i++)
+		for (int j = 0; j < _boardHeight; j++) {
+			var cell = Instantiate(_cellPrefab, new Vector2(i, j), Quaternion.identity);
+			cell.name = "Cell_" + i + "_" + j;
+			_cells[i, j] = cell.GetComponent<Cell>();
+		}
+	}
+
+	private void Update() {
+		if (Input.GetMouseButton(0)) {
+			RaycastHit hit;
+			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        
+			if (Physics.Raycast(ray, out hit)) {
+				Transform objectHit = hit.transform;
+				Debug.Log("Hitted");
+				if (!_isCellSelected) {
+					_isCellSelected = true;
+					_currentlySelected = new Vector2Int((int)objectHit.position.x, (int)objectHit.position.y);
+					_cells[_currentlySelected.x, _currentlySelected.y].ToggleSelection((true));
+
+				}
+				else if (_isCellSelected) {
+					_cells[_currentlySelected.x, _currentlySelected.y].ToggleSelection((false));
+					_currentlySelected = new Vector2Int((int)objectHit.position.x, (int)objectHit.position.y);
+					_cells[_currentlySelected.x, _currentlySelected.y].ToggleSelection((true));
+				}
+					
+			
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+	[SerializeField] private Sprite _imageEmpty;
+	[SerializeField] private Sprite _imageFilled;
+	[SerializeField] private Sprite _imageDestroyed;
+	[SerializeField] private Sprite _imageSelected;
+
+	private SpriteRenderer _sr;
+
+	private void Start() {
+		_sr = GetComponent<SpriteRenderer>();
+	}
+
+	public void ToggleSelection(bool selected) {
+		_sr.sprite = selected ? _imageSelected : _imageEmpty;
+	}
+}
